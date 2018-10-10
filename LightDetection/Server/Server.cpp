883b6070c6919d3f06de27000ac31af4d9bf1b83
wangh <1163505981@qq.com>
2018-10-10 22:24:20 +0800

    Mod:服务器启动按钮功能

--- LightDetection/Server/Server.cpp
+++ LightDetection/Server/Server.cpp
@@ -8,13 +8,28 @@ Server::Server(QWidget *parent)
     m_iPort = 80;
 
     connect(ui.pushButton_start, SIGNAL(clicked()), this, SLOT(pushButton_start_slot()));
+    // connect(ui.lineEdit_port, SIGNAL(textChanged(QString)), this, SLOT(lineEdit_port_slot(QString)));
 }
 
 void Server::pushButton_start_slot()
 {
     QString strPort = ui.lineEdit_port->text();
     m_iPort = strPort.toInt();
-    ui.statusBar->showMessage(u8"服务端启动，监听端口" + strPort);
+    if (m_iPort == 0)
+    {
+        ui.statusBar->showMessage(u8"请设置正确的端口!");
+    }
+    m_sock = UdpServer(m_iPort);
+    if (m_sock.startBind() == false)
+    {
+        ui.statusBar->showMessage(u8"Init or Bind Error!");
+    }
+
+    ui.statusBar->showMessage(u8"服务端启动，监听端口:" + strPort);
 }
 
-
+void Server::lineEdit_port_slot(QString strText)
+{
+    m_iPort = strText.toInt();
+    ui.statusBar->showMessage(u8"监听端口已改为：" + QString::number(m_iPort));
+}
--- LightDetection/Server/Server.h
+++ LightDetection/Server/Server.h
@@ -2,6 +2,7 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_Server.h"
+#include "UdpServer.h"
 
 class Server : public QMainWindow
 {
@@ -13,7 +14,9 @@ public:
 private:
     Ui::ServerClass ui;
     int m_iPort; // ¼ÇÂ¼¶Ë¿ÚºÅ
+    UdpServer m_sock;
 
 private slots:
     void pushButton_start_slot();
+    void lineEdit_port_slot(QString strText);
 };
